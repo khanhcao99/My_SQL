64a30d995d7d36e9f4a19273ac6a26319272f90a
khanhcao99 <khanhboy1324@gmail.com>
2022-07-18 15:59:54 +0700

    'mysqlsampledatabase.sql

--- mysqlsampledatabase.sql
+++ mysqlsampledatabase.sql
@@ -7943,3 +7943,82 @@ end //
 delimiter ;
 
 call findAllCustomer();
+
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+SELECT * FROM customers where customerNumber = 175;
+
+END //
+DELIMITER ;
+
+delimiter //
+DROP PROCEDURE IF EXISTS `getCusById`//
+create procedure getCusById(in cusNum int )
+begin
+select * from customers where customerNumber = cusNum;
+end //
+delimiter ;
+
+call getCusById(175);
+
+DELIMITER //
+
+CREATE PROCEDURE GetCustomersCountByCity(
+
+    IN  in_city VARCHAR(50),
+
+    OUT total INT
+
+)
+
+BEGIN
+
+    SELECT COUNT(customerNumber)
+
+    INTO total
+
+    FROM customers
+
+    WHERE city = in_city;
+
+END//
+
+DELIMITER ;
+
+call GetCustomersCountByCity('Lyon', @total);
+select @total;
+
+DELIMITER //
+
+CREATE PROCEDURE SetCounter(
+
+    INOUT counter INT,
+
+    IN inc INT
+
+)
+
+BEGIN
+
+    SET counter = counter + inc;
+
+END//
+
+DELIMITER ;
+
+SET @counter = 1;
+
+CALL SetCounter(@counter,1); -- 2
+
+CALL SetCounter(@counter,1); -- 3
+
+CALL SetCounter(@counter,5); -- 8
+
+SELECT @counter; -- 8
+
